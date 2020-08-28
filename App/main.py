688b0745adc18d473c67952da12165d52f20724e
ryankoul <52511004+ryankoul@users.noreply.github.com>
2020-08-27 20:51:19 -0400

    MVP

--- App/main.py
+++ App/main.py
@@ -2,18 +2,17 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
-from App.Api import predict, viz
+from App.Api import predict
 
 app = FastAPI(
     title='Who is the Saltiest Hacker?',
     description="""Identifying and ranking negative commenters on 
     Hacker News using sentiment analysis.""",
-    version='0.1.8',
+    version='0.1.9.7',
     docs_url='/',
 )
 
 app.include_router(predict.router)
-app.include_router(viz.router)
 
 app.add_middleware(
     CORSMiddleware,
